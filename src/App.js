12c4a1905e182e76de7a9f80bb39dbfe90d967cd
Sabrina Aytac <sabrinasaytac@gmail.com>
2020-06-29 22:51:07 +0100

    Format

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class App extends Component {
-  render() {
-    return <div>Immersive Labs Frontend Technical Test</div>;
-  }
-}
-
-export default App;
--- src/components/App/App.styles.js
+++ src/components/App/App.styles.js
@@ -1,14 +1,15 @@
 import styled from 'styled-components';
 
-export const Input = styled.input`
-  padding: 0.5em;
-  margin: 0.5em;
-  background: rgba(0, 0, 0, 0.5);
-  border: none;
-  border-radius: 1px;
-  :focus {
-    background: rgba(0, 0, 0, 0);
-  }
+export const SearchInput = styled.input`
+padding: 0.5em;
+margin: 0.5em;
+background: rgba(255, 255, 255, 0.5);
+border: none;
+border-radius: 1px;
+outline: none;
+:focus {
+  background: rgba(255, 255, 255, 1);
+}
 `;
 
 export const AppContainer = styled.div`
