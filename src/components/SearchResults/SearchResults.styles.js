6f433bb538375ceea60716f0cce495f7758bec5e
Sabrina Aytac <sabrinasaytac@gmail.com>
2020-06-30 00:13:13 +0100

    Add styled user list components

---
+++ src/components/SearchResults/SearchResults.styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const UserTable = styled.table`
+  width: 100%;
+  padding: 20px;
+`;
+
+export const UserRow = styled.tr`
+  width: 100%;
+`;
+
+export const AvatarColumn = styled.td`
+  width: 40%;
+  text-align: right;
+`;
+
+export const NameColumn = styled.td`
+  width: 60%;
+  text-align: left;
+`;
