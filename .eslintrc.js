3abfd75ac7af1e34a82148824a041688582034e7
Sabrina Aytac <sabrinasaytac@gmail.com>
2020-06-29 21:24:24 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'import/prefer-default-export': 0,
+    'react/jsx-props-no-spreading': 0,
+  },
+};
