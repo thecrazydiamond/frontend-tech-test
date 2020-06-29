bc24fc931a7c9f2cd4bf879efee5797bf7b9d288
Sabrina Aytac <sabrinasaytac@gmail.com>
2020-06-30 00:33:43 +0100

    Implement search results

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
+import debounce from 'lodash/debounce';
 
 import { AppContainer, SearchResultContainer, SearchInput } from './App.styles';
+import SearchResults from '../SearchResults';
 
 const App = () => (
-
-  //   const onChange = React.useMemo(() => { target: { value } }) => {
-
-  // },
-
   <AppContainer>
     <SearchInput
       type="text"
       placeholder="Name"
       onFocus={(e) => { e.target.placeholder = ''; }}
       onBlur={(e) => { e.target.placeholder = 'Name'; }}
-    // onChange={onChange}
     />
-    <SearchResultContainer />
+    <SearchResultContainer>
+      <SearchResults query={null} onUserClick={null} />
+    </SearchResultContainer>
   </AppContainer>
 );
-
 export default App;
---
+++ src/components/SearchResults/SearchResults.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useQuery } from '@apollo/react-hooks';
+
+import { LIST_ALL_USERS, LIST_USERS_BY_NAME } from '../../graphql/users';
+import {
+  UserTable, UserRow, AvatarColumn, NameColumn,
+} from './SearchResults.styles';
+
+const SearchResults = ({ query, onUserClick }) => {
+  const { loading, error, data } = useQuery(
+    query
+      ? LIST_USERS_BY_NAME
+      : LIST_ALL_USERS,
+    {
+      variables: {
+        searchQuery: query,
+      },
+    },
+  );
+
+  return (
+    <div>
+      {!!loading && <p>Searching</p>}
+      {!loading && !data?.users.length && <p>No results</p>}
+      {!loading && !!error && <p>{`${error}`}</p>}
+      {
+        !loading
+        && (
+          <UserTable>
+            {data?.users.map((user) => (
+              <UserRow
+                key={`user-${user.name}`}
+                style={{ width: '100%', marginTop: '10px' }}
+                onClick={() => onUserClick(user)}
+              >
+                <AvatarColumn>
+                  {user.avatar}
+                </AvatarColumn>
+                <NameColumn>{user.name}</NameColumn>
+              </UserRow>
+            ))}
+          </UserTable>
+        )
+      }
+    </div>
+  );
+};
+
+SearchResults.propTypes = {
+  query: PropTypes.string.isRequired,
+  onUserClick: PropTypes.func.isRequired,
+};
+
+export default SearchResults;
